3a998e940b7714b6f0276577dc44d8608df82203
TanvirTurja <43377119+TanvirTurja@users.noreply.github.com>
2021-09-20 11:07:42 +0600

    new changes

--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,48 @@
-
 alert('hello, js');
+console.log('1');
+
+let age = 25;
+let year = 2010;
+
+console.log(age , year);
+
+age = 30;
+console.log(age);
+
+const points = 100;
+// points = 40; doesn't work
+console.log(points);
+
+var score = 75;
+console.log(score);
+
+//strings
+console.log('hello, world');
+let email = 'mario@gmail.com'
+console.log(email);
+//string concatenation
+
+let firstName = 'Brandon';
+let lastName = 'Sanderson';
+
+let fullName = firstName + ' ' + lastName ;
+console.log(fullName);
+
+//getting characters
+
+console.log(fullName[0]);
+console.log(fullName[2]);
+
+//string length
+
+console.log(fullName.length);
+
+// string methods
+
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+
+console.log(result , fullName);
+
+let index = email.indexOf('@');
+console.log(index);
